5b46e967fad9cdee1aff6057c162588981ccea56
huzaifak08 <hk7928042@gmail.com>
2023-05-24 16:16:21 +0500

    Sign in using bloc done Allhamdullillah

--- lib/Authentication/auth_bloc.dart
+++ lib/Authentication/auth_bloc.dart
@@ -1,5 +1,4 @@
 import 'package:bloc_statemanagement/Authentication/auth_service.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
